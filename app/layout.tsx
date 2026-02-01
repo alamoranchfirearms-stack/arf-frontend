ddab5b31d060db3b29c290b8da96d22b44c8b288
William Durand <alamoranchfirearms@gmail.com>
2026-02-01 08:23:02 -0600

    Use Nav component in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
-import HeaderNav from "./components/HeaderNav";
+import Nav from "./components/Nav";
 
 
 const geistSans = Geist({
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <HeaderNav />
+        <Nav />
         {children}
         <Analytics />
       </body>
